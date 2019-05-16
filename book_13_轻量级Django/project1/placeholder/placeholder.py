8abd1536c496ffaab9ac5243268d5ac47762190c
sweetpotato <justsweetpotato@gmail.com>
2019-05-16 19:32:09 +0800

    使用缓存减小带宽消耗

--- book_13_轻量级Django/project1/placeholder/placeholder.py
+++ book_13_轻量级Django/project1/placeholder/placeholder.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import sys
 from django.conf import settings
@@ -24,6 +25,10 @@ from django import forms
 from django.conf.urls import url
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.wsgi import get_wsgi_application
+from io import BytesIO
+from PIL import Image, ImageDraw
+from django.core.cache import cache
+from django.views.decorators.http import etag
 
 
 class ImageForm(forms.Form):
@@ -32,14 +37,39 @@ class ImageForm(forms.Form):
     height = forms.IntegerField(min_value=1, max_value=2000)
     width = forms.IntegerField(min_value=1, max_value=2000)
 
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}\nHello World'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
+        return content
 
+
+def generate_etag(request, width, height):
+    content = 'Placeholder: {} x {}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+
+@etag(generate_etag)
 def placeholder(request, width, height):
     form = ImageForm({'height': height, 'width': width})
     if form.is_valid():
-        height = form.cleaned_data['height']
-        width = form.cleaned_data['width']
-        # TODO: Generate image of requested size
-        return HttpResponse('OK, width: {}, height: {}'.format(width, height))
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
     else:
         return HttpResponseBadRequest('Invalid Image Request')
 
