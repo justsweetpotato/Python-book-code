02c7a49a06fb03ff2d9b9fb525c0299bf21b6ac4
sweetpotato <justsweetpotato@gmail.com>
2019-05-16 17:26:13 +0800

    添加 ALLOWED_HOSTS

--- book_13_轻量级Django/project1/hello.py
+++ book_13_轻量级Django/project1/hello.py
@@ -1,9 +1,17 @@
+import os
 import sys
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY="thisisthesecretkey",
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
