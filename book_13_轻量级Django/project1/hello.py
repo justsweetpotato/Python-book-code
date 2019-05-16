631a348fdd9798ae0829b37c27d271cf70e11d2e
sweetpotato <justsweetpotato@gmail.com>
2019-05-16 17:10:11 +0800

    第一个 Hello World 程序

---
+++ book_13_轻量级Django/project1/hello.py
@@ -0,0 +1,30 @@
+import sys
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY="thisisthesecretkey",
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h3>Hello World<h3>")
+
+
+urlpatterns = (
+    url(r"^$", index),
+)
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
