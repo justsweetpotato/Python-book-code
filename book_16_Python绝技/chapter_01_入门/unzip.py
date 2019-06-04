917be1a59d8ebbbb0806d5b7b464870c3878ae6e
sweetpotato <justsweetpotato@gmail.com>
2019-06-05 00:58:51 +0800

    暴力破解

--- book_16_Python绝技/chapter_01_入门/unzip.py
+++ book_16_Python绝技/chapter_01_入门/unzip.py
@@ -4,10 +4,11 @@ zFile = zipfile.ZipFile('evil.zip')
 passFile = open('dictionary.txt')
 for line in passFile.readlines():
     password = line.strip('\n')
-    print('Try password: ' + password + '.')
+    # print('Try password: ' + password + '.', end="\n")
     try:
-        zFile.extractall(pwd=password)
+        zFile.extractall(pwd=password.encode())
         print('[+] Password = ' + password + '\n')
+        zFile.close()
         exit(0)
-    except Exception, e:
+    except Exception as e:
         pass
